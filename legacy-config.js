a3242c707fda4d39135412fb024d33cba35e0770
dlerm <younglerman@gmail.com>
2019-04-08 15:55:10 -0700

    Webpack config update - legacy scripts

--- legacy-config.js
+++ legacy-config.js
@@ -8,22 +8,29 @@ const scriptPaths = `${path.resolve('./src/assets/scripts/legacy')}/**/*.js`;
 const scriptName = process.env.LEGACY_SCRIPT_NAME || 'legacy';
 const scriptFilename = `${scriptName}.js.liquid`;
 
-const minLegacySingleScriptsPlugin = new CopyWebpackPlugin([{
-  to: '[name].min.[ext]',
-  from: { glob: scriptPaths },
-  flatten: true,
-  transform: (code) => babel.transform(code, { presets: ['minify'] }).code,
-}]);
+const minLegacySingleScriptsPlugin = new CopyWebpackPlugin(
+  [
+    {
+      to: '[name].min.[ext]',
+      from: { glob: scriptPaths },
+      flatten: true,
+      transform: (code) => babel.transform(code, { presets: ['minify'] }).code,
+    }
+  ]
+);
 
-const minLegacyMegaScriptPlugin = new MergeIntoSingleFilePlugin({
-  files: [{
-    src: glob.sync(scriptPaths),
-    dest: (code) => {
-      const data = {};
-      data[scriptFilename] = babel.transform(code, { presets: ['minify'] }).code;
-      return data;
-    },
-  }]
+const minLegacyMegaScriptPlugin = new MergeIntoSingleFilePlugin(
+{
+  files: [
+    {
+      src: glob.sync(scriptPaths),
+      dest: (code) => {
+        const data = {};
+        data[scriptFilename] = babel.transform(code, { presets: ['minify'] }).code;
+        return data;
+      },
+    }
+  ]
 });
 
 module.exports = {
